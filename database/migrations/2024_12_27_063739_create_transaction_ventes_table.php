57bde38fcd23287edb16b8649e24cba5106c2cdd
Dylan429-eng <sohdylan84@gmail.com>
2024-12-27 13:49:30 +0100

    ajout d'une nouvelle table pour stocker les operations de vente

---
+++ database/migrations/2024_12_27_063739_create_transaction_ventes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionVentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_ventes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produit');
+            $table->tinyInteger('serveur');
+            $table->Integer('quantite');
+            $table->Integer('prix');
+            $table->Integer('total_ventes');
+            $table->date('date_vente');
+            $table->timestamps();
+            $table->foreign('produit')->references('code_produit')->on('produit_fixes')
+            ->onDelete('cascade');
+            $table->foreign('serveur')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_ventes');
+    }
+}
