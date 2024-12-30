02aa50415ef68064376c4b245382feaba0c71b99
Dylan429-eng <sohdylan84@gmail.com>
2024-12-30 11:16:15 +0100

    controller message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Message;
 class MessageController extends Controller
 {
     public function message() {
@@ -13,10 +13,21 @@ class MessageController extends Controller
         }
         return view('pages.message');
     }
-    public function store_message() {
+    public function store_message(Request $request) {
+        $employe = auth()->user();
         if (!$employe) {
             return redirect()->route('login')->with('error', 'Veuillez vous connecter');
         }
-        
+        $request->validate([
+         'category'=>'required',
+         'message'=>'required|string|max:1000',
+        ]);
+        Message::create([
+          'message'=>$request->message,
+          'type'=>$request->category,
+          'date_message'=>now(),
+          'name'=>$employe->name
+        ]);
+        return redirect()->route('dashboard')->with('Succes','Message envoye avec succes');
     }
 }
