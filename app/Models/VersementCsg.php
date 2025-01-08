5a408edd8d0f24aced8bd892a4caad649c06d28e
Dylan429-eng <sohdylan84@gmail.com>
2025-01-08 11:06:07 +0100

    model versemntCsg

--- app/Models/VersementCsg.php
+++ app/Models/VersementCsg.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class VersementCsg extends Model
 {
+    use HasFactory;
     protected $table = 'Versement_csg';
+    protected $primaryKey='code_vcsg';
+    protected $fillable = [
+        'libelle',
+        'date',
+        'somme',
+        'verseur',
+        'encaisseur',
+        'created_at',
+        'updated_at'
+    ];
 }
+
